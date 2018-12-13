9ba27f481b44f98ca1914b1f56ca12562d269cf0
Vladyslav <Alksgest@gmail.com>
2018-12-13 15:57:47 +0200

    Added deadlock with semaphore

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,60 @@
 package com.company;
 
+import java.util.concurrent.Semaphore;
+
 public class Main {
-    static public final String lockObject1 = "LockObject 1";
-    static public final String lockObject2 = "LockObject 2";
+    static public Semaphore semaphore;
 
     public static Integer mainResource = 1;
 
     public static void main(String[] args) {
-
+        semaphore = new Semaphore(1);
         Thread consumer = new Thread(new Consumer());
         Thread producer = new Thread(new Producer());
 
         producer.start();
         consumer.start();
 
+
     }
 }
 
 class Consumer implements Runnable {
-
-
     @Override
     public void run() {
-        synchronized (Main.lockObject1) {
-            try {
-                synchronized (Main.mainResource) {
-                    while (Main.mainResource > 0) {
-                        Thread.sleep(200);
-                        --Main.mainResource;
-                        System.out.printf("Consumer : resources count : %d\n", Main.mainResource);
-                    }
+        try {
+            Main.semaphore.acquire();
+                while (Main.mainResource > 0) {
+                    Thread.sleep(200);
+                    --Main.mainResource;
+                    System.out.printf("Consumer : resources count : %d\n", Main.mainResource);
                 }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            synchronized (Main.lockObject2) {
-                System.out.println(Main.lockObject2);
-            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        Main.semaphore.release();
+
     }
 }
+
 
 class Producer implements Runnable {
 
     @Override
     public void run() {
-        synchronized (Main.lockObject2) {
-            try {
-                synchronized (Main.mainResource) {
-                    while (Main.mainResource < 20) {
-                        Thread.sleep(100);
-                        ++Main.mainResource;
-                        System.out.printf("Producer : resources count : %d\n", Main.mainResource);
-                    }
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        try {
+            Main.semaphore.acquire();
+            while (Main.mainResource < 20) {
+                Thread.sleep(100);
+                ++Main.mainResource;
+                System.out.printf("Producer : resources count : %d\n", Main.mainResource);
             }
-            synchronized (Main.lockObject1) {
-                System.out.println(Main.lockObject1);
-            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        //Main.semaphore.release();
+        System.out.println("Method release() has not invoked");
     }
 }
